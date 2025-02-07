7c841461f905e50e61416bf8b8ee4d9ad290d31a
Kelvin Goredema <goredemakelvin3@gmail.com>
2025-02-07 21:09:53 +0200

    init

---
+++ src/test/java/com/cicosy/crm/TestCrmApplication.java
@@ -0,0 +1,11 @@
+package com.cicosy.crm;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CrmApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
