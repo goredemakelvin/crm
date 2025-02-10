e655d75591c285fb1db381472fc17911f43d9ef3
Kelvin Goredema <goredemakelvin3@gmail.com>
2025-02-10 10:42:12 +0200

    added @Id in Email Address

--- src/main/java/com/cicosy/crm/entity/EmailAddress.java
+++ src/main/java/com/cicosy/crm/entity/EmailAddress.java
@@ -1,11 +1,13 @@
 package com.cicosy.crm.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class EmailAddress {
-
+	
+    @Id
 	private Long id;
 	private String email;
 	private boolean enabled;
